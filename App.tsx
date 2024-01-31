bf2c1cf837336bafdefd62fd5d64ea5e1353ef05
Khalif <anupamborboruah100@gmail.com>
2024-02-01 00:51:10 +0530

    added the ui and set up

---
+++ App.tsx
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+import React from 'react';
+// import type {PropsWithChildren} from 'react';
+// import {Text} from 'react-native';
+
+
+import SignIn from './components/signIn';
+
+
+
+function App() {
+
+
+  return (
+    <SignIn />
+  );
+}
+
+
+export default App;
